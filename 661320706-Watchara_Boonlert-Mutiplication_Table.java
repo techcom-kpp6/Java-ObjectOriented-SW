11920c9c70a35891f7abc88638305c8d796cb8af
techcom-kpp6 <git239615@gmail.com>
2025-11-11 23:14:07 +0700

    Update 661320706-Watchara_Boonlert-Mutiplication_Table.java

--- 661320706-Watchara_Boonlert-Mutiplication_Table.java
+++ 661320706-Watchara_Boonlert-Mutiplication_Table.java
@@ -1,19 +1,18 @@
-public class Main{
+public class Watchara_Boonlert_Mutiplication_Table_661320706{
 	public static void main(String[] args) {
-	    int [][]Multi_tb = new int[13][13];
-	    int sum, mother = 12;
-	    for (int i=1; i<=mother; i++){
-	        System.out.printf("สูตรคูณแม่ %d\n\n",i);
-	        for (int o=1; o<=mother; o++){
-	            sum = i*o;
-	            Multi_tb[i][o] = sum;
-	            System.out.printf("%d x %d = %d\n", i, o, Multi_tb[i][o]);
-	            sum = 0;
-	        }
-	        System.out.println();
-	    }
-	}
+	int [][]Multi_tb = new int[13][13];
+        System.out.printf("-------------------------661320706-------------------------------------\n\n");
+        for(int p=1; p<=12; p++){
+            System.out.printf("|M%3d ", p);
+        }
+        System.out.printf("\n-----------------------------------------------------------------------\n");
+        for(int i=1; i<Multi_tb.length; i++){
+            for(int o=1; o<Multi_tb.length; o++){
+                Multi_tb[i][o] = i*o;
+                        System.out.printf("| %3d ", Multi_tb[i][o]);
+            }
+            System.out.println();
+        }
+        System.out.printf("\n---------------------Watchara Boonlert---------------------------------\n");
+    }
 }
-
-
-
